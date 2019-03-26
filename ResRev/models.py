26c8de2636a68d3cf304e2e5ba94b79444515815
Dxcuer <thanapatkjm@hotmail.com>
2019-03-26 23:13:54 +0700

    Work with model without adding imageField

--- ResRev/models.py
+++ ResRev/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    contact = models.CharField(max_length=80)
+    description = models.TextField()
+    category = models.TextField()
+    rating = models.FloatField(default=0)
+    g_rating = models.FloatField(default=0)
+    # photo
+
+class Reviewer(models.Model):
+    name = models.CharField(max_length=30)
+    like = models.IntegerField()
+    clas = models.TextField()
+    description = models.TextField()
+    contact = models.CharField(max_length=80)
+    hours = models.IntegerField()
+    password = models.CharField(max_length=20)
+    # photo
+class Review(models.Model):
+    restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    owner = models.ForeignKey(Reviewer,on_delete=models.CASCADE)
+    like = models.IntegerField()
+    rating = models.FloatField(validators=[MinValueValidator(1.0), MaxValueValidator(5.0)],)
+    comment = models.TextField()
